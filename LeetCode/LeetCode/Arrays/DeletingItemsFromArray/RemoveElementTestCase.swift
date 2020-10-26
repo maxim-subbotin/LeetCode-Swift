cd97e61c6acf271cf2c24ee5dd67b8582188edaf
Maxim Subbotin <msubbotin@snappii.com>
2020-10-26 10:58:44 +0300

    Remove Element

--- LeetCode/LeetCode/Arrays/DeletingItemsFromArray/RemoveElementTestCase.swift
+++ LeetCode/LeetCode/Arrays/DeletingItemsFromArray/RemoveElementTestCase.swift
@@ -25,28 +25,26 @@ public class RemoveElementTestCase: TestCase {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
         var index = 0
         var length = nums.count
-        var count = 0
         while index < length {
             if nums[index] == val {
                 nums.remove(at: index)
-                count += 1
                 length -= 1
             } else {
                 index += 1
             }
         }
-        return count
+        return length
     }
     
     public static func test() -> Bool {
         let testData = [3,2,2,3,0,3,5,5,3,3,0,5,5]
-        let v = 3
+        let v = 0
         // [3,2,2,3], 3
         // [0,1,2,2,3,0,4,2], 2
         // [3,2,2,3,0,3,5,5,3,3,0,5,5], 3
         let test = RemoveElementTestCase(withArray: testData, andValue: v)
         test.run()
         
-        return true
+        return test.result == 11 && test.array == [3,2,2,3,3,5,5,3,3,5,5]
     }
 }
