b427c51cafe0fabb87ad746e2b6cb09f8d12bb31
Maxim Subbotin <msubbotin@snappii.com>
2020-10-27 13:21:30 +0300

    Find All Numbers Disappeared in an Array

---
+++ LeetCode/LeetCode/Arrays/Conclusion/FindDisappearedNumbersTestCase.swift
@@ -0,0 +1,54 @@
+//
+//  FindDisappearedNumbersTestCase.swift
+//  LeetCode
+//
+//  Created by Snappii on 10/27/20.
+//
+
+import Foundation
+
+public class FindDisappearedNumbersTestCase: TestCase {
+    public var name: String = "Find All Numbers Disappeared in an Array"
+    var array: [Int]
+    var result = [Int]()
+    
+    init(withArray arr: [Int]) {
+        self.array = arr
+    }
+    
+    public func run() {
+        result = findDisappearedNumbers(array)
+    }
+    
+    func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+        var arr = nums
+        var missedNumbers = [Int]()
+        for i in 0..<arr.count {
+            let val = arr[i]
+            let index = abs(val) - 1
+            if arr[index] > 0 { // not marked yet
+                arr[index] *= -1
+            }
+        }
+        for i in 0..<arr.count {
+            if arr[i] > 0 {
+                missedNumbers.append(i + 1)
+            }
+        }
+        
+        return missedNumbers
+    }
+    
+    func swap(from i: Int, to j: Int, in array: inout [Int]) {
+        let a = array[j]
+        array[j] = array[i]
+        array[i] = a
+    }
+    
+    public static func test() -> Bool {
+        let testData = [4,3,2,7,8,2,3,1]
+        let test = FindDisappearedNumbersTestCase(withArray: testData)
+        test.run()
+        return test.result == [5,6]
+    }
+}
--- LeetCode/LeetCodeConsole/main.swift
+++ LeetCode/LeetCodeConsole/main.swift
@@ -11,6 +11,6 @@ import LeetCode
 runTest()
 
 func runTest() {
-    let res = ThirdMaximumNumberTestCase.test()
+    let res = FindDisappearedNumbersTestCase.test()
     print(res)
 }
